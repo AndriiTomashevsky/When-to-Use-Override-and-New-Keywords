5a573f006d637d48484959986aba4c1885566568
AndriiTomashevsky <tomashevskiy.a@gmail.com>
2021-05-13 10:33:55 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Tutorial see https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/classes-and-structs/knowing-when-to-use-override-and-new-keywords
+namespace ConsoleApp1
+{
+    class BaseClass
+    {
+        // for step 4 add the virtual modifier
+        public void Method1()
+        {
+            Console.WriteLine("Base - Method1");
+        }
+
+        // Step 2 - uncomment Method2, uncomment calling in Main. Addition of the Method2 method in BaseClass causes a warning.
+        //public void Method2()
+        //{
+        //    Console.WriteLine("Base - Method2");
+        //}
+    }
+
+    class DerivedClass : BaseClass
+    {
+        // Step 3 - add the new modifier. The new keyword preserves the relationships that produce output in step 2, but it suppresses the warning.
+        public void Method2()
+        {
+            Console.WriteLine("Derived - Method2");
+        }
+
+        // Step 4 - uncomment Method1, add the virtual modifier to base Method1
+        //public override void Method1()
+        //{
+        //    Console.WriteLine("Derived - Method1");
+        //}
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseClass bc = new BaseClass();
+            DerivedClass dc = new DerivedClass();
+            BaseClass bcdc = new DerivedClass();
+
+            bc.Method1();            // Base - Method1  
+            dc.Method1();            // Base - Method1    // Derived - Method1 (after step 4)
+            dc.Method2();            // Derived - Method2
+            bcdc.Method1();          // Base - Method1    // Derived - Method1 (after step 4). 
+            #region The use of the override modifier enables bcdc to access the Method1 that is defined in DerivedClass.
+            // Typically, that is the desired behavior in inheritance hierarchies. 
+            // You want objects that have values that are created from the derived class to use the methods that are defined in the derived class. 
+            // You achieve that behavior by using override to extend the base class method.
+            #endregion
+
+
+            // uncomment for step 2
+            //bc.Method2();          // Base - Method2
+            //bcdc.Method2();        // Base - Method2
+
+            Console.ReadKey();
+        }
+    }
+}
